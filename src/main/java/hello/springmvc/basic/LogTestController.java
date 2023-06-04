a662677c74c4279c1c1567e7d4c2e006dfa6a9ed
kimseunghyun-kr <incongnito12@gmail.com>
2023-06-05 01:38:55 +0900

    loggers

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,12 +1,15 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController //directly returns val on HTTP message
 public class LogTestController {
+//    @Slf4j in lombok writes the following auto
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/log-test")
@@ -17,12 +20,16 @@ public class LogTestController {
         System.out.println("name" + name);
 
         //        diff levels of logging, least severe to most severe
-        log.trace("  info log = {} , {}", name, name);
-        log.debug("  info log = {}", name);
-        log.warn("  info log =  {}", name);
+        log.trace("  trace log = {} , {}", name, name);
+        log.debug("  debug log = {}", name);
         log.info("  info log = {}", name);
-        log.error("  info log = {}", name);
+        log.warn("  warn log =  {}", name);
+        log.error("  error log = {}", name);
 
+//      do not do this. even if the log is not printed, the string concat will still occur
+//      slows down process - eager evaluation is bad in this case
+//        log.info("  info log " + name);
+//        use the lazy eval version with {} above
 
         return "ok";
     }
